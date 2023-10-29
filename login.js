f09b7b721a06f1f8ac257f6357c218bf90024758
Anuj232002 <birhadeanuj2002@gmail.com>
2023-10-29 21:57:20 +0530

    first

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const express = require("express");
 const bodyParser = require("body-parser");
-const encoder = bodyParser.urlencoded();
+const encoder = bodyParser.urlencoded({ extended: true });
+
+// import { final_HASH } from "./SHA512.js";
 
 const app = express();
 app.use("/assets", express.static("assets"));
@@ -9,8 +11,8 @@ app.use("/assets", express.static("assets"));
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "aditya",
-  database: "CNS",
+  password: "root",
+  database: "users",
 });
 
 // connect to the database
@@ -24,16 +26,29 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", encoder, function (req, res) {
+  // var username = req.body.username;
+  // var password = req.body.password;
+  // generateHASH512(password);
+  // var passkey = encrypt(req.body.password);
+  // console.log(passkey);
+
   var username = req.body.username;
   var password = req.body.password;
-  var passkey = encrypt(req.body.password);
-  console.log(passkey);
+
+  module.exports = { password };
+  var SHA256 = require("./SHA512");
+  var HASH = SHA256.final_HASH;
+
+  console.log(username);
+  console.log(HASH);
+
   connection.query(
-    "select * from user_log_in_details where user_email = ? and user_pass = ?",
-    [username, password],
+    "select * from member where user_email = ? and user_pass = ?",
+    [username, HASH],
     function (error, results, fields) {
-      if (results.length > 0) {
-        // console.log(results[0].user_pass);
+      console.log(results[0].user_pass);
+      if (results.length > 0 && results[0].user_pass == HASH) {
+        console.log("Hash matched successfully !!!!!");
         res.redirect("/welcome");
       } else {
         console.log("Invalid Username or Password");
@@ -44,18 +59,26 @@ app.post("/", encoder, function (req, res) {
   );
 });
 
-function encrypt(passpass) {
-  return passpass + "1" + "2" + "3";
-}
+// function encrypt(passpass) {
+//   return passpass + "1" + "2" + "3";
+// }
 
 app.post("/register", encoder, function (req, res) {
   var name = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
 
+  module.exports = { password };
+  var SHA256 = require("./SHA512");
+  var HASH = SHA256.final_HASH;
+
+  console.log(name);
+  console.log(email);
+  console.log(HASH);
+
   connection.query(
-    "INSERT INTO user_log_in_details (user_name, user_email, user_pass) VALUES (?, ?, ?)",
-    [name, email, password],
+    "INSERT INTO member (user_name, user_email, user_pass) VALUES (?, ?, ?)",
+    [name, email, HASH],
     function (error, results, fields) {
       if (error) {
         console.log("Error registering user: " + error.message);
